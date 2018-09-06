61ce3a0334a32f1c4f62000d73ad16acfb878a61
JackieAlb <xiegz@163.com>
2018-09-06 15:46:19 +0800

    initial by Jackie

---
+++ src/main/java/top/th/jackie/study/WeixinDemoEndApplication.java
@@ -0,0 +1,12 @@
+package top.th.jackie.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinDemoEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinDemoEndApplication.class, args);
+	}
+}
