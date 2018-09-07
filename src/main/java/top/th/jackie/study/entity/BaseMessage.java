56fe93f9f815b0bc856b65831845603980fb427b
JackieAlb <xiegz@163.com>
2018-09-07 23:25:30 +0800

    refactor BaseMessage

--- src/main/java/top/th/jackie/study/entity/BaseMessage.java
+++ src/main/java/top/th/jackie/study/entity/BaseMessage.java
@@ -2,39 +2,45 @@ package top.th.jackie.study.entity;
 
 public class BaseMessage {
 
-	public static class Message {
-		public String toUserName;
-		public String fromUserName;
-		public long createTime;
-		public String msgType;
+		protected String toUserName;
+		protected String fromUserName;
+		protected long createTime;
+		protected String msgType;
 
-		public Message() {
+		public BaseMessage() {
+			super();
 		}
-	}
-	protected Message data = new Message();
-	public String getToUserName() {
-		return data.toUserName;
-	}
-	public void setToUserName(String toUserName) {
-		this.data.toUserName = toUserName;
-	}
-	public String getFromUserName() {
-		return data.fromUserName;
-	}
-	public void setFromUserName(String fromUserName) {
-		this.data.fromUserName = fromUserName;
-	}
-	public long getCreateTime() {
-		return data.createTime;
-	}
-	public void setCreateTime(long createTime) {
-		this.data.createTime = createTime;
-	}
-	public String getMsgType() {
-		return data.msgType;
-	}
-	public void setMsgType(String msgType) {
-		this.data.msgType = msgType;
-	}
+
+		public String getToUserName() {
+			return toUserName;
+		}
+
+		public void setToUserName(String toUserName) {
+			this.toUserName = toUserName;
+		}
+
+		public String getFromUserName() {
+			return fromUserName;
+		}
+
+		public void setFromUserName(String fromUserName) {
+			this.fromUserName = fromUserName;
+		}
+
+		public long getCreateTime() {
+			return createTime;
+		}
+
+		public void setCreateTime(long createTime) {
+			this.createTime = createTime;
+		}
+
+		public String getMsgType() {
+			return msgType;
+		}
+
+		public void setMsgType(String msgType) {
+			this.msgType = msgType;
+		}
 	
 }
